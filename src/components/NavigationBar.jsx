48b0a4c6218e1adc74df1018045d5fa42909620a
Amritha043 <amrithaa043@gmail.com>
2026-07-09 23:50:26 +0530

    updating

---
+++ src/components/NavigationBar.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const NavigationBar = () => {
+  return (
+    <div>
+<nav class="navbar navbar-expand-lg   border-subtle border-bottom border-body">
+  <div class="container-fluid">
+    <a class="navbar-brand " href="#">APPLICATION FORM</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active " aria-current="page" href="#">ADD EMPLOYEE</a>
+        </li>
+        
+        <li class="nav-item">
+          <a class="nav-link active " href="#">SEARCH EMPLOYEE</a>
+        </li>
+        
+        
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+
+
+    </div>
+  )
+}
+
+export default NavigationBar
