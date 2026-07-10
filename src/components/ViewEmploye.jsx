9273b32bfe93866f54d6f4a644473deb7da6735f
Amritha043 <amrithaa043@gmail.com>
2026-07-10 11:17:16 +0530

    dynamically displayed table view

--- src/components/ViewEmploye.jsx
+++ src/components/ViewEmploye.jsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const ViewEmploye = () => {
+  const [data,changeData]=useState(
+    [
+     
+      {"name":"Aarav Sharma","code":"EMP101","phone":9876543210,"desg":"Software Engineer"},
+      {"name":"Ananya Nair","code":"EMP102","phone":9123456789,"desg":"HR Executive"},
+      {"name":"Rahul Kumar","code":"EMP103","phone":9988776655,"desg":"Project Manager"},
+      {"name":"Sneha Joseph","code":"EMP104","phone":9876501234,"desg":"UI/UX Designer"},
+      {"name":"Aditya Singh","code":"EMP105","phone":9765432109,"desg":"System Analyst"},
+      {"name":"Meera Menon","code":"EMP106","phone":9898989898,"desg":"Network Engineer"},
+    ]
+  )
   return (
     <div>
 
@@ -18,75 +29,24 @@ const ViewEmploye = () => {
           </thead>
 
           <tbody>
-            <tr>
-              <td>Aarav Sharma</td>
-              <td>EMP101</td>
-              <td>9876543210</td>
-              <td>Software Engineer</td>
+            {
+              data.map(
+                (value,index)=>{
+                  return(
+                    <tr>
+              <td>{value.name}</td>
+              <td>{value.code}</td>
+              <td>{value.phone}</td>
+              <td>{value.desg}</td>
             </tr>
+                  )
+                }
+              )
+            }
+            
 
-            <tr>
-              <td>Ananya Nair</td>
-              <td>EMP102</td>
-              <td>9123456789</td>
-              <td>HR Executive</td>
-            </tr>
-
-            <tr>
-              <td>Rahul Kumar</td>
-              <td>EMP103</td>
-              <td>9988776655</td>
-              <td>Project Manager</td>
-            </tr>
-
-            <tr>
-              <td>Sneha Joseph</td>
-              <td>EMP104</td>
-              <td>9876501234</td>
-              <td>UI/UX Designer</td>
-            </tr>
-
-            <tr>
-              <td>Aditya Singh</td>
-              <td>EMP105</td>
-              <td>9765432109</td>
-              <td>System Analyst</td>
-            </tr>
-
-            <tr>
-              <td>Meera Menon</td>
-              <td>EMP106</td>
-              <td>9898989898</td>
-              <td>Accountant</td>
-            </tr>
-
-            <tr>
-              <td>Arjun Das</td>
-              <td>EMP107</td>
-              <td>9012345678</td>
-              <td>Network Engineer</td>
-            </tr>
-
-            <tr>
-              <td>Diya Thomas</td>
-              <td>EMP108</td>
-              <td>9871234567</td>
-              <td>Marketing Executive</td>
-            </tr>
-
-            <tr>
-              <td>Rohan Patel</td>
-              <td>EMP109</td>
-              <td>9123098765</td>
-              <td>Data Analyst</td>
-            </tr>
-
-            <tr>
-              <td>Aisha Khan</td>
-              <td>EMP110</td>
-              <td>9988123456</td>
-              <td>Business Analyst</td>
-            </tr>
+           
+            
           </tbody>
         </table>
       </div>
